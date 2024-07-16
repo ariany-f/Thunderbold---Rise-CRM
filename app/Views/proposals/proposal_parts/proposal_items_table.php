16cf4eecfd3a4c089af32198757e771b7587b216
ariany-f <ariany_f@hotmail.com>
2024-07-16 19:24:39 -0300

    Ajuste qtd e total

--- app/Views/proposals/proposal_parts/proposal_items_table.php
+++ app/Views/proposals/proposal_parts/proposal_items_table.php
@@ -17,22 +17,22 @@ $total_after_discount_row = '<tr>
 
 <table class="table-responsive" style="width: 100%; color: #444;">            
     <tr style="font-weight: bold; background-color: <?php echo $color; ?>; color: #fff;  ">
-        <th colspan="2" style="width: 45%; border-right: 1px solid #eee;"> <?php echo app_lang("item"); ?> </th>
+        <th colspan="2" style="width: 55%; border-right: 1px solid #eee;"> <?php echo app_lang("item"); ?> </th>
         <!--<th style="text-align: right;  width: 20%; border-right: 1px solid #eee;"> <?php //echo app_lang("rate"); ?></th>-->
-        <th style="text-align: center;  width: 15%; border-right: 1px solid #eee;"> <?php echo app_lang("quantity"); ?></th>
-        <th style="text-align: right;  width: 20%; "> <?php echo app_lang("total"); ?></th>
+        <th style="text-align: center;  width: 10%; border-right: 1px solid #eee;"> <?php echo app_lang("quantity"); ?></th>
+        <th style="text-align: right;  width: 15%; "> <?php echo app_lang("total"); ?></th>
     </tr>
     <?php
     foreach ($proposal_items as $item) {
         ?>
         <tr style="background-color: #f4f4f4; ">
-            <td colspan="2" style="width: 45%; border: 1px solid #fff; padding: 10px;"><?php echo $item->title; ?>
+            <td colspan="2" style="width: 55%; border: 1px solid #fff; padding: 10px;"><?php echo $item->title; ?>
                 <br />
                 <span style="color: #888; font-size: 90%;"><?php echo nl2br($item->description ? process_images_from_content($item->description) : ""); ?></span>
             </td>
             <!--<td style="text-align: right; width: 20%; border: 1px solid #fff;"> <?php echo to_currency($item->rate, $item->currency_symbol); ?></td>-->
-            <td style="text-align: center; width: 15%; border: 1px solid #fff;"> <?php echo $item->quantity + $item->quantity_gp  . " " . $item->unit_type; ?></td>
-            <td style="text-align: right; width: 20%; border: 1px solid #fff;"> <?php echo to_currency($item->total, $item->currency_symbol); ?></td>
+            <td style="text-align: center; width: 10%; border: 1px solid #fff;"> <?php echo $item->quantity + $item->quantity_gp  . " " . $item->unit_type; ?></td>
+            <td style="text-align: right; width: 15%; border: 1px solid #fff;"> <?php echo to_currency($item->total, $item->currency_symbol); ?></td>
         </tr>
     <?php } ?>
     <tr>
