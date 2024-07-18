c836310f7347f3b5ef6cc7d9382d1ce7a73e07ae
ariany-f <ariany_f@hotmail.com>
2024-07-18 14:41:08 -0300

    Ajuste total proposta

--- app/Views/proposals/proposal_parts/proposal_items_table.php
+++ app/Views/proposals/proposal_parts/proposal_items_table.php
@@ -38,7 +38,7 @@ $total_after_discount_row = '<tr>
     <tr>
         <td colspan="2" style="text-align: right;"><?php echo app_lang("sub_total"); ?></td>
         <td style="text-align: center; width: 10%; border: 1px solid #fff; background-color: #f4f4f4;">
-            <?php echo $proposal_total_summary->proposal_total_quantity; ?>
+            <?php echo $proposal_total_summary->proposal_sum_total_quantity; ?>
         </td>
         <td style="text-align: right; width: 15%; border: 1px solid #fff; background-color: #f4f4f4;">
             <?php echo to_currency($proposal_total_summary->proposal_subtotal, $proposal_total_summary->currency_symbol); ?>
