ea1bc16f2e00ae7f771551aeb6bfb4be55b2bc1c
ariany-f <ariany_f@hotmail.com>
2024-10-02 10:05:21 -0300

    teste

--- app/Views/messages/edit_message_modal_form.php
+++ app/Views/messages/edit_message_modal_form.php
@@ -44,21 +44,24 @@
 
         <?php if (!empty($model_info->files)) : ?>
             var existingFiles = <?php echo json_encode(unserialize($model_info->files)); ?>; // Converte os arquivos existentes em JSON
-            
+           
             existingFiles.forEach(function(file) {
                 var mockFile = { name: file.file_name, size: file.size }; // Cria um objeto mock do arquivo
-                dropzone.emit("addedfile", mockFile);  // Simula que o arquivo foi adicionado ao Dropzone
+                // dropzone.emit("addedfile", mockFile);  // Simula que o arquivo foi adicionado ao Dropzone
                 
-                // Ajuste o caminho da miniatura se necessário
-                var thumbnailUrl = "<?php echo get_uri('uploads/messages/' . $model_info->id . '/'); ?>" + file.file_name;
+                // // Ajuste o caminho da miniatura se necessário
+                <?php $target_path = get_setting("timeline_file_path");?>
+                var thumbnailUrl = "<?php echo get_uri($target_path); ?>" + file.file_name;
                 
-                // Se o arquivo for uma imagem, carregue a miniatura
-                if (file.is_image) {  // Supondo que exista uma propriedade 'is_image' indicando se é uma imagem
-                    dropzone.emit("thumbnail", mockFile, thumbnailUrl); // Exibe a miniatura
-                }
+                // // Se o arquivo for uma imagem, carregue a miniatura
+                // if (file.is_image) {  // Supondo que exista uma propriedade 'is_image' indicando se é uma imagem
+                //     dropzone.emit("thumbnail", mockFile, thumbnailUrl); // Exibe a miniatura
+                // }
                 
-                dropzone.emit("complete", mockFile);  // Marca o arquivo como completo
-                dropzone.files.push(mockFile);  // Adiciona o arquivo à lista de arquivos do Dropzone
+                // dropzone.emit("complete", mockFile);  // Marca o arquivo como completo
+                // dropzone.files.push(mockFile);  // Adiciona o arquivo à lista de arquivos do Dropzone
+                dropzone.displayExistingFile(mockFile, thumbnailUrl);
+
             });
         <?php endif; ?>
 
