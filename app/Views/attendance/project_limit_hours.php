969477718695c56fee70c536950feef79fad999f
ariany-f <ariany_f@hotmail.com>
2024-10-14 13:56:41 -0300

    Exibição limite não definido

--- app/Views/attendance/project_limit_hours.php
+++ app/Views/attendance/project_limit_hours.php
@@ -5,7 +5,7 @@
                 <i data-feather="clock" class="icon"></i>
             </div>
             <div class="w-100 text-end">
-                <?php if($limit == 0) { ?>
+                <?php if($limit == '00:00:00') { ?>
                     <h3><?php echo app_lang('not_set'); ?></h3>
                 <?php } else { ?>
                     <h1><?php echo $limit; ?></h1>
