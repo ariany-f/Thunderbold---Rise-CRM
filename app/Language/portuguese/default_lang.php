919d7d0fe491f0099628215ac8cbe443d991f968
ariany-f <ariany_f@hotmail.com>
2024-10-31 03:23:21 -0300

    Tradução

--- app/Language/portuguese/default_lang.php
+++ app/Language/portuguese/default_lang.php
@@ -251,6 +251,7 @@ $lang["all_team_members"] = "Todos os membros da equipe";
 $lang["all_team_clients"] = "Todos os clientes";
 $lang["start_timesheet_filter"] = "Início filtro Horas registradas";
 $lang["end_timesheet_filter"] = "Fim filtro Horas registradas";
+$lang["generate_invoice"] = "Gerar Fatura";
 
 /* settings */
 $lang["app_settings"] = "Configurações do Sistema";
