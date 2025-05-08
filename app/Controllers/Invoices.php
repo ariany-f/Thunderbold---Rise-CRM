f81d16effddabac5dd7be90887f88ab3d2d345c0
ariany-f <ariany_f@hotmail.com>
2025-05-08 13:19:01 -0300

    Ajuste invoices

--- app/Controllers/Invoices.php
+++ app/Controllers/Invoices.php
@@ -558,9 +558,9 @@ class Invoices extends Security_Controller {
         $project_icon = ' <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid icon"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>';
 
         if($data->is_ticket && $data->project_title) {
-            $project_title = $ticket_icon . $data->project_title;
+            $project_title = $ticket_icon . ' ' . $data->project_title;
         } else if($data->project_title) {
-            $project_title = $project_icon . $data->project_title;
+            $project_title = $project_icon . ' ' . $data->project_title;
         } else {
             $project_title = "-";
         }
