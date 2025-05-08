4189456c41a8f348fee9f2bc1400824bacb8febe
ariany-f <ariany_f@hotmail.com>
2025-05-08 13:20:09 -0300

    invoices

--- app/Controllers/Invoices.php
+++ app/Controllers/Invoices.php
@@ -569,7 +569,7 @@ class Invoices extends Security_Controller {
             $data->id,
             $invoice_url,
             anchor(get_uri("clients/view/" . $data->client_id), $data->company_name),
-            $data->project_title ? anchor(get_uri("projects/view/" . $data->project_id), $project_title) : "-",
+            $data->project_title ? anchor(get_uri("projects/view/" . $data->project_id . ($data->is_ticket ? "/ticket" : "")), $project_title) : "-",
             $data->bill_date,
             format_to_date($data->bill_date, false),
             $data->due_date,
